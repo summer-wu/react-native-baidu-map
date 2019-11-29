e9ca499d4130b18d0134e6310c0313e06f330d2e
giveme5 <286674783@qq.com>
2019-11-29 12:09:16 +0800

    更新GeolocationModule.java

--- android/src/main/java/org/lovebing/reactnative/baidumap/GeolocationModule.java
+++ android/src/main/java/org/lovebing/reactnative/baidumap/GeolocationModule.java
@@ -1,10 +1,20 @@
+/**
+ * Copyright (c) 2016-present, lovebing.org.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package org.lovebing.reactnative.baidumap;
 
+import android.Manifest;
 import android.util.Log;
+
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.location.LocationClientOption.LocationMode;
 import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.search.core.PoiInfo;
 import com.baidu.mapapi.search.core.SearchResult;
@@ -15,21 +25,26 @@ import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
 import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
 import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 import com.baidu.mapapi.utils.CoordinateConverter;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
+import org.lovebing.reactnative.baidumap.support.AppUtils;
+
 import java.util.List;
 
 /**
  * Created by lovebing on 2016/10/28.
  */
 public class GeolocationModule extends BaseModule
-    implements BDLocationListener, OnGetGeoCoderResultListener {
+        implements BDLocationListener, OnGetGeoCoderResultListener {
 
   private LocationClient locationClient;
   private static GeoCoder geoCoder;
+  private volatile boolean locating = false;
+  private volatile boolean locateOnce = false;
 
   public GeolocationModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -40,26 +55,26 @@ public class GeolocationModule extends BaseModule
     return "BaiduGeolocationModule";
   }
 
-  private void initLocationClient() {
+  private void initLocationClient(String coorType) {
+    AppUtils.checkPermission(context.getCurrentActivity(), Manifest.permission.ACCESS_FINE_LOCATION);
     LocationClientOption option = new LocationClientOption();
-    option.setCoorType("bd09ll");
+    option.setLocationMode(LocationMode.Hight_Accuracy);
+    option.setCoorType(coorType);
     option.setIsNeedAddress(true);
     option.setIsNeedAltitude(true);
     option.setIsNeedLocationDescribe(true);
     option.setOpenGps(true);
-    option.disableCache(true);
     locationClient = new LocationClient(context.getApplicationContext());
     locationClient.setLocOption(option);
     Log.i("locationClient", "locationClient");
     locationClient.registerLocationListener(this);
   }
-
   /**
    *
    * @return
    */
   protected GeoCoder getGeoCoder() {
-    if (geoCoder != null) {
+    if(geoCoder != null) {
       geoCoder.destroy();
     }
     geoCoder = GeoCoder.newInstance();
@@ -78,33 +93,69 @@ public class GeolocationModule extends BaseModule
     converter.coord(sourceLatLng);
     LatLng desLatLng = converter.convert();
     return desLatLng;
+
   }
 
   @ReactMethod
-  public void getCurrentPosition() {
+  public void convertGPSCoor(double lat, double lng, Promise promise) {
+    Log.i("convertGPSCoor", "convertGPSCoor");
+    LatLng latLng = getBaiduCoorFromGPSCoor(new LatLng(lat, lng));
+    WritableMap map = Arguments.createMap();
+    map.putDouble("latitude", latLng.latitude);
+    map.putDouble("longitude", latLng.longitude);
+    promise.resolve(map);
+  }
+
+  @ReactMethod
+  public void getCurrentPosition(String coorType) {
+    if (locating) {
+      return;
+    }
+    locateOnce = true;
+    locating = true;
     if (locationClient == null) {
-      initLocationClient();
+      initLocationClient(coorType);
     }
     Log.i("getCurrentPosition", "getCurrentPosition");
     locationClient.start();
   }
 
   @ReactMethod
+  public void startLocating(String coorType) {
+    if (locating) {
+      return;
+    }
+    locateOnce = false;
+    locating = true;
+    initLocationClient(coorType);
+    locationClient.start();
+  }
+
+  @ReactMethod
+  public void stopLocating() {
+    locating = false;
+    if (locationClient != null) {
+      locationClient.stop();
+      locationClient = null;
+    }
+  }
+
+  @ReactMethod
   public void geocode(String city, String addr) {
     getGeoCoder().geocode(new GeoCodeOption()
-        .city(city).address(addr));
+            .city(city).address(addr));
   }
 
   @ReactMethod
   public void reverseGeoCode(double lat, double lng) {
     getGeoCoder().reverseGeoCode(new ReverseGeoCodeOption()
-        .location(new LatLng(lat, lng)));
+            .location(new LatLng(lat, lng)));
   }
 
   @ReactMethod
   public void reverseGeoCodeGPS(double lat, double lng) {
     getGeoCoder().reverseGeoCode(new ReverseGeoCodeOption()
-        .location(getBaiduCoorFromGPSCoor(new LatLng(lat, lng))));
+            .location(getBaiduCoorFromGPSCoor(new LatLng(lat, lng))));
   }
 
   @Override
@@ -112,6 +163,7 @@ public class GeolocationModule extends BaseModule
     WritableMap params = Arguments.createMap();
     params.putDouble("latitude", bdLocation.getLatitude());
     params.putDouble("longitude", bdLocation.getLongitude());
+    params.putDouble("speed", bdLocation.getSpeed());
     params.putDouble("direction", bdLocation.getDirection());
     params.putDouble("altitude", bdLocation.getAltitude());
     params.putDouble("radius", bdLocation.getRadius());
@@ -127,8 +179,15 @@ public class GeolocationModule extends BaseModule
     params.putString("buildingId", bdLocation.getBuildingID());
     params.putString("buildingName", bdLocation.getBuildingName());
     Log.i("onReceiveLocation", "onGetCurrentLocationPosition");
-    sendEvent("onGetCurrentLocationPosition", params);
-    locationClient.stop();
+
+    if (locateOnce) {
+      locating = false;
+      sendEvent("onGetCurrentLocationPosition", params);
+      locationClient.stop();
+      locationClient = null;
+    } else {
+      sendEvent("onLocationUpdate", params);
+    }
   }
 
   @Override
@@ -136,9 +195,10 @@ public class GeolocationModule extends BaseModule
     WritableMap params = Arguments.createMap();
     if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
       params.putInt("errcode", -1);
-    } else {
-      params.putDouble("latitude", result.getLocation().latitude);
-      params.putDouble("longitude", result.getLocation().longitude);
+    }
+    else {
+      params.putDouble("latitude",  result.getLocation().latitude);
+      params.putDouble("longitude",  result.getLocation().longitude);
     }
     sendEvent("onGetGeoCodeResult", params);
   }
@@ -148,7 +208,8 @@ public class GeolocationModule extends BaseModule
     WritableMap params = Arguments.createMap();
     if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
       params.putInt("errcode", -1);
-    } else {
+    }
+    else {
       ReverseGeoCodeResult.AddressComponent addressComponent = result.getAddressDetail();
       params.putString("address", result.getAddress());
       params.putString("province", addressComponent.province);
@@ -156,22 +217,19 @@ public class GeolocationModule extends BaseModule
       params.putString("district", addressComponent.district);
       params.putString("street", addressComponent.street);
       params.putString("streetNumber", addressComponent.streetNumber);
-      List<PoiInfo> poiList = result.getPoiList();
-      WritableArray poiArray = Arguments.createArray();
-      if(poiList != null) {
-        for (PoiInfo poiInfo : poiList) {
-          WritableMap poiMap = Arguments.createMap();
-          poiMap.putString("uid", poiInfo.uid);
-          poiMap.putString("name", poiInfo.name);
-          poiMap.putString("address", poiInfo.address);
-          poiMap.putDouble("latitude", poiInfo.location.latitude);
-          poiMap.putDouble("longitude", poiInfo.location.longitude);
-          poiMap.putString("city", poiInfo.city);
 
-          poiArray.pushMap(poiMap);
-        }
+      WritableArray list = Arguments.createArray();
+      List<PoiInfo> poiList = result.getPoiList();
+      for (PoiInfo info: poiList) {
+        WritableMap attr = Arguments.createMap();
+        attr.putString("name", info.name);
+        attr.putString("address", info.address);
+        attr.putString("city", info.city);
+        attr.putDouble("latitude", info.location.latitude);
+        attr.putDouble("longitude", info.location.longitude);
+        list.pushMap(attr);
       }
-      params.putArray("poiList", poiArray);
+      params.putArray("poiList", list);
     }
     sendEvent("onGetReverseGeoCodeResult", params);
   }
